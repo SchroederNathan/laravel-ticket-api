79eaa41b5b68227f8a3594ddb2dee50c8f5e8e23
“Nathan <schroedernathan2011@icloud.com>
2025-03-05 17:22:02 -0500

    Including adn ommitting data

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\V1\TicketController;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\V1\UsersController;
 use Illuminate\Support\Facades\Route;
 
 // http://localhost:8000/api
@@ -13,10 +14,10 @@ Route::post('/register', [AuthController::class, 'register']);
 // Version 1 API routes
 Route::prefix('v1')->group(function () {
     Route::middleware('auth:sanctum')->apiResource('tickets', controller: TicketController::class);
+    Route::middleware('auth:sanctum')->apiResource('users', controller: UsersController::class);
 
     Route::post('/login', [AuthController::class, 'login']);
     Route::post('/register', [AuthController::class, 'register']);
-    
+
     Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
-    // Add other v1-specific routes here
 });
