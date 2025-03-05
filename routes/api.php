20abc7dd25acc88219066c3c81ac7fb5a6e3fe92
“Nathan <schroedernathan2011@icloud.com>
2025-03-05 01:16:50 -0500

    Added API versioning

--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,21 @@
 <?php
 
+use App\Http\Controllers\Api\V1\TicketController;
 use App\Http\Controllers\AuthController;
 use App\Models\Ticket;
 use Illuminate\Support\Facades\Route;
 
 // http://localhost:8000/api
 
-
+// Non-versioned API routes
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
 
-Route::get('/tickets', function () {
-    return Ticket::all();
+// Version 1 API routes
+Route::prefix('v1')->group(function () {
+    Route::apiResource('tickets', controller: TicketController::class);
+
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/register', [AuthController::class, 'register']);
+    // Add other v1-specific routes here
 });
