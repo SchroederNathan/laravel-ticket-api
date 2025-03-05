9841bca2fd39d3a11ace903f38cdcbaccc85b5bf
“Nathan <schroedernathan2011@icloud.com>
2025-03-04 23:59:34 -0500

    Created APIResponses

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,15 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Traits\ApiResponses;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    public function login() {
+    use ApiResponses;
+    public function login()
+    {
+        return $this->ok('Hello, Login');
 
-            return response()->json([
-                'message' => 'Hello, Login'
-            ], 200);
-            
     }
 }
