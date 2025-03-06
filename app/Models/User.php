ab9204ff0b92ea5f6d45cdbf4ebd9bd96225429f
“Nathan <schroedernathan2011@icloud.com>
2025-03-05 20:10:12 -0500

    Added sorting methods to query builders

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Http\Filters\V1\QueryFilter;
+use Illuminate\Contracts\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -50,4 +52,9 @@ class User extends Authenticatable
         return $this->hasMany(Ticket::class);
     }
 
+
+    public function scopeFilter(Builder $builder, QueryFilter $filters)
+    {
+        return $filters->apply($builder);
+    }
 }
